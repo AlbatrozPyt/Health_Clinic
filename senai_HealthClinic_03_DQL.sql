47cd83bfe81044f0757fdb03a36ebe1ee24689d7
Matheus Enrike Ramalho Ortiz <matheus@senai>
2023-08-15 17:04:44 -0300

    Arrumando de novo

--- senai_HealthClinic_03_DQL.sql
+++ senai_HealthClinic_03_DQL.sql
@@ -3,9 +3,11 @@ select
 	Clinica.NomeFantasia as [Nome Da Clinica],
 	Paciente.Endereco as [Endereco do Paciente],
 	Medico.Especializacao as [Especialização],
+	Medico.CRM as [CRM do medico],
 	Usuario.NomeUsuario as [Nome do Paciente],
 	Paciente.Idade as [Idade do Paciente],
 	Paciente.Telefone as [Telefone do Paciente],
+	Consulta.IdConsulta as [Id da Consulta],
 	Consulta.DataConsulta as [Data da Consulta],
 	Consulta.HoraConsulta as [Hora da Consulta],
 	FeedBack.Descricao as [Comentario do Paciente]
@@ -15,7 +17,7 @@ inner join Consulta on Medico.IdMedico = Consulta.IdConsulta
 inner join Paciente on Paciente.IdPaciente = Consulta.IdPaciente
 inner join Usuario on Usuario.IdUsuario = Medico.IdMedico 
 left join FeedBack on FeedBack.IdConsulta = Consulta.IdConsulta
-
+where Usuario.IdUsuario = Medico.IdMedico
 
 --Criar função para retornar a quantidade de médicos de uma determinada especialidade
 select COUNT(IdMedico) as [Numero de Medicos] from Medico
